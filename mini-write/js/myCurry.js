a4e8ea0c96f86303ae3fdfa72c4e3bdf59a91e13
fangdown <409951398@qq.com>
2020-03-12 18:59:28 +0800

    '增加动画'

--- mini-write/js/myCurry.js
+++ mini-write/js/myCurry.js
@@ -1,50 +1,30 @@
-function myCurry(fn) {
-  var args = [...arguments].slice(1);
-  return function () {
-    if (arguments.length === 0) {
-      return fn.apply(this, args); // 没传参数时，调用这个函数
-    } else {
-      [].push.apply(args, arguments); // 传入了参数，把参数保存下来
-      return arguments.callee; // 返回这个函数的引用
-    }
-  }
-}
-
-// demo
-function add(x, y) {
-  return x + y
-}
-const result = myCurry(add)
-console.log('result: ', result(1)(2)());
-
-
-function multiFn(a, b, c) {
-  return a * b * c;
-}
-var multi = myCurry(multiFn);
-let res = multi(2, 3, 4);
-let res1 = multi(2)(3, 4);
-console.log('res', res())
-console.log('res1: ', res1());
-
-
+/**
+ * 柯里化
+ * 确定函数的参数个数
+ * 参数个数=外层传参+ 执行传参
+ * 如果小于总参数则调用call继续curry,返回函数
+ * 否则apply执行函数，返回结果值
+ * @param {*} fn
+ * @param {*} args
+ */
 function curry(fn, args) {
-  var length = fn.length;
-  var args = args || [];
-  return function () {
+  var length = fn.length // 函数参数的个数
+  var args = args || []
+  return function() {
     newArgs = args.concat(Array.prototype.slice.call(arguments))
-    console.log(newArgs)
     if (newArgs.length < length) {
-      return curry.call(this, fn, newArgs);
+      return curry.call(this, fn, newArgs)
     } else {
-      return fn.apply(this, newArgs);
+      return fn.apply(this, newArgs)
     }
   }
 }
 function multiFn1(a, b, c) {
-  console.log(a * b * c)
-  return a * b * c;
+  return a * b * c
 }
 
-var multi = curry(multiFn1);
-multi(2)(3)(4)
+var multi = curry(multiFn1)
+let r1 = multi(2)(3)(4)
+let r2 = multi(2)(3)
+console.log(r1) // 24
+console.log(r2) // [Function]
